f55dc155884954c50ffd36a5884473ee3a00d070
TKM1994 <92644025+TKM1994@users.noreply.github.com>
2024-01-23 13:43:32 +0530

    documentation added in config file

--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -1,20 +1,23 @@
+# importing necessary library
 import os
 
 
+# authentication security related variable
 SECURITY_PROTOCOL="SASL_SSL"
 SSL_MACHENISM="PLAIN"
 
+# confluent cloud kafka api related variable
 API_KEY = os.getenv('API_KEY',None)
 API_SECRET_KEY = os.getenv('API_SECRET_KEY',None)
 BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER',None)
 
+
+# schema related variable
 ENDPOINT_SCHEMA_URL  = os.getenv('ENDPOINT_SCHEMA_URL',None)
-
-
 SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY',None)
 SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
 
-
+# kafka cluster configruation
 def sasl_conf():
 
     sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
@@ -27,7 +30,7 @@ def sasl_conf():
     return sasl_conf
 
 
-
+# schema configruation
 def schema_config():
 
     schema_config = {'url':ENDPOINT_SCHEMA_URL,
@@ -36,6 +39,8 @@ def schema_config():
                     }
 
     return schema_config
+
+
 
 if __name__ == '__main__':
     print(sasl_conf())
