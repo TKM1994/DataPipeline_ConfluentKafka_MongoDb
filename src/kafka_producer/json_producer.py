06db787ff16b1bf5b4b59c7a824b38850396a929
TKM1994 <92644025+TKM1994@users.noreply.github.com>
2024-01-24 13:26:48 +0530

    writing some explanation

--- src/kafka_producer/json_producer.py
+++ src/kafka_producer/json_producer.py
@@ -28,19 +28,18 @@ def produce_data_using_file(topic,file_path):
     logging.info(f"Topic: {topic} file_path:{file_path}")
     schema_str = Generic.get_schema_to_produce_consume_data(file_path=file_path)
     schema_registry_conf = schema_config()
-    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
-    string_serializer = StringSerializer('utf_8')
-    json_serializer = JSONSerializer(schema_str, schema_registry_client, instance_to_dict)
-    # serialize instances of the Generic class to JSON format using the schema and serialization callback.
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)  #A Confluent Schema Registry client
+    string_serializer = StringSerializer('utf_8')      #Serializes unicode to bytes per the configured codec. Defaults to utf_8
+    json_serializer = JSONSerializer(schema_str, schema_registry_client, instance_to_dict)    #JSON Serializer that outputs JSON encoded data with Confluent Schema Registry framing
     producer = Producer(sasl_conf())
 
     producer.poll(0.0)
     try:
-        for instance in Generic.get_object(file_path=file_path):
+        for instance in Generic.get_object(file_path=file_path):  # getting each row of csv file as object
             logging.info(f"Topic: {topic} file_path:{instance.to_dict()}")
             producer.produce(topic=topic,
                              key=string_serializer(str(uuid4()), instance.to_dict()),
-                             value=json_serializer(instance, SerializationContext(topic, MessageField.VALUE)),
+                             value=json_serializer(instance, SerializationContext(topic, MessageField.VALUE)), # it Serialize Json data which we get from instance_to_dict into confluent schema registry framing 
                              on_delivery=delivery_report)
             print("\nFlushing records...")
             producer.flush()
